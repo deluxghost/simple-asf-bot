755aa07fa6b2fc11e52c96421cc37f665f394ac6
Ghost Liu <deluxghost@gmail.com>
2017-09-22 12:23:43 +0800

    update @all pesudo user

--- bot.py
+++ bot.py
@@ -13,8 +13,16 @@ import sys
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 pattern_2fa = re.compile(r'^\s*!?2[fF][aA]( +.+)?\s*$')
+pattern_sa = re.compile(r'^<(.+)>', re.M)
+pattern_all = re.compile(r'@all\b')
 
 def asf_ipc(command):
+    command_all = pattern_all.search(command)
+    if command_all:
+        ret = list()
+        for player in get_players():
+            ret.append(asf_ipc(command.replace('@all', player)))
+        return ''.join(ret).replace('\n\n', '\n')
     ipc_url = ipc + parse.quote(command)
     req = request.Request(ipc_url)
     try:
@@ -27,6 +35,11 @@ def asf_ipc(command):
         res = res_data.read().decode("utf-8")
         return res
 
+def get_players():
+    msg = asf_ipc('sa')
+    players = pattern_sa.findall(msg)
+    return players
+
 def mfa_timeout(bot, job):
     bot.editMessageText(chat_id=job.context[0], message_id=job.context[1], text='[2FA Deleted]')
 
@@ -37,7 +50,10 @@ def reply(bot, update, job_queue):
     chat_id = update.message.chat_id
     command = update.message.text
     if admin == str(chat_id):
-        msg = update.message.reply_text(asf_ipc(command), quote=True)
+        res = asf_ipc(command)
+        if not isinstance(res, str):
+            res = str(res)
+        msg = update.message.reply_text(res, quote=True)
         if pattern_2fa.match(command):
             job_queue.run_once(mfa_timeout, 15, context=(chat_id, msg.message_id))
     else:
